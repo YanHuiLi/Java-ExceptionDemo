ed71570f9002399ba550771e83db05d7e9d9a6d6
Archer Lee <yanhui_0831@foxmail.com>
2016-03-23 16:02:53 +0800

    异常的try catch解决办法

--- src/com/archer/src/ExceptionDemo01.java
+++ src/com/archer/src/ExceptionDemo01.java
@@ -6,21 +6,35 @@ package com.archer.src;
 
 
 class Exc{
-    int i=10;
+    int a=10;
+    int b=10;
 }
 
 public class ExceptionDemo01 {
     public static void main(String[] args) {
-        int a=10;
-        int b=0;
-        int temp = 0;
 
+        int temp = 0;
+        Exc e=null;
+        e=new Exc();
         try {
-            temp = a/b;
-        } catch (ArithmeticException e) {
-            e.printStackTrace();
+            temp=e.a/e.b;
+            System.out.println(temp);
+            //出现异常以后没办法成功退出了
+
+        } catch (NullPointerException e1) {
+            System.out.println("空指针异常"+e1);
+        }catch (ArithmeticException e1){
+            System.out.println("算术异常"+e1);
+
         }
-        System.out.println(temp);
+        //finally不管异常还是不异常都是退出的
+        finally {
+            System.out.println("程序退出");
+        }
+
+
+
+
 
     }
 }
